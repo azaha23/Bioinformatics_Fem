da6834b4e822f7c77081ad559e4fa53cb46df149
azaha23 <44067464+azaha23@users.noreply.github.com>
2019-05-23 23:08:19 +0300

    Add files via upload

--- bioinf.py
+++ bioinf.py
@@ -10,69 +10,34 @@ file_fasta = open(argv[1].lower() + "_nuc.fasta", "w")
 for line in lines:
     line = str(line)
     if line[2:6] == "ATOM" and line[15] == "P":
-        print(line)
         read.append(line)
 
 url_file.close()
 
-dna = []
-rna = []
+na = []
+f = 0
 
 for elem in read:
-    if elem[21] == "A" or elem[21] == "C" or elem[21] == "G" or elem[21] == "U":
-        rna.append(elem)
+    if elem[21] == "T":
+        f = 1
+        na.append(elem)
+    elif elem[21] == "A" or elem[21] == "C" or elem[21] == "G":
+        na.append(elem)
+
+lst_na = []
+
+for el in na:
+    if el[23] not in lst_na:
+        lst_na.append(el[23])
+
+for p in lst_na:
+    file_fasta.write(">" + argv[1] + "_" + p + " ")
+    if f == 1:
+        file_fasta.write("DNA\n")
     else:
-        dna.append(elem)
+        file_fasta.write("RNA\n")
 
-lst_dna = []
-
-for el in dna:
-    if el[23] not in lst_dna:
-        lst_dna.append(el[23])
-
-for p in lst_dna:
-    file_fasta.write(">" + argv[1] + "_" + p + " " + "DNA\n")
-    for el in dna:
+    for el in na:
         if el[23] == p:
             file_fasta.write(el[21])
     file_fasta.write("\n")
-
-lst_rna = []
-
-for els in rna:
-    if els[23] not in lst_rna:
-        lst_rna.append(els[23])
-
-for q in lst_rna:
-    file_fasta.write(">" + argv[1] + "_" + q + " " + "RNA\n")
-    for els in rna:
-        if els[23] == q:
-            file_fasta.write(els[21])
-    file_fasta.write("\n")
-
-file_fasta.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
